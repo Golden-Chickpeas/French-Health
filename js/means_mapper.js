64c6b847b15df4b31b567bdcafbdf38edbe0682e
VZ <valentinazelayam@gmail.com>
2018-01-02 22:33:42 +0100

    begginning with visualisation update and menu links

---
+++ js/means_mapper.js
@@ -0,0 +1,14 @@
+/**
+* means_mapper.js contains all necessary functions for the
+* update of the map's visualised data, which are usually means of some kind
+*
+* @file Contains the necessary functions for the home page behaviour
+* @author Valentina Zelaya & Timothy Garwood
+* @version 1.0
+*/
+//HISTORY : January 1st 2018 - Initial design and coding (@vz-chameleon)
+
+
+function updateMeansMap(category){
+  alert("Updating means with :"+category);
+}
