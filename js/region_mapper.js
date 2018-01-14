b0df6484dfc01f3e4e3d65333a597f8248801690
VZ <valentinazelayam@gmail.com>
2018-01-14 04:31:11 +0100

    [region_viz_barchart] Beginning of hierarchical barchart

--- js/region_mapper.js
+++ js/region_mapper.js
@@ -56,11 +56,12 @@ function goto_region(d,reg_num){
 
     // First tab : circle packing
     region_foodgrp_hierarchy_data='data/json/foodgrp_sougrp_conso_week/region'+reg_num+'/consos_grp_'+window.selected_foodgrp+'_region_'+reg_num+'.json'
-    load_circle_packing(region_foodgrp_hierarchy_data)
+    load_circle_packing(region_foodgrp_hierarchy_data);
+    load_hierarchical_barchart(region_foodgrp_hierarchy_data);
 
     //Second tab : Icicle
     region_foodgrp_partition_data='data/json/foodgrp_conso_partition/region'+reg_num+'/consos_partition_'+window.selected_foodgrp+'_region_'+reg_num+'.json'
-    load_icicle(region_foodgrp_partition_data)
+    load_icicle(region_foodgrp_partition_data);
 
 
   } else {
@@ -71,7 +72,7 @@ function goto_region(d,reg_num){
     closeVisualisation('week_circle_pack');
     closeVisualisation('icicle_container');
     displayVisualisationAccessBar(false);
-    var reg_descrip=document.getElementById('description');
+    var reg_descrip=document.getElementById(' description');
     reg_descrip.innerHTML='<h3> Santé et habitudes alimentaires en France</h3>';
   }
 
@@ -94,7 +95,7 @@ function load_circle_packing(data_file_path){
     d3.select('#circle_pack').remove();
 
     var svg = d3.select("#week_circle_pack").append("svg")
-    .attr("id", "circle_pack") .attr("width",width/2.5) .attr("height",height/1.4),
+    .attr("id", "circle_pack") .attr("width",width/3) .attr("height",height/1.4),
     margin = 2,
     diameter = +svg.attr("width"),
     g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
@@ -437,4 +438,227 @@ function load_icicle(data_file_path){
           .text(percentageString);
     }
 
+}
+
+
+
+function load_hierarchical_barchart(data_file_path){
+      var x = d3.scaleLinear()
+        .range([0, width/3]);
+
+      var barHeight = 20;
+
+      var color = d3.scaleOrdinal()
+          .range(["steelblue", "#ccc"]);
+
+      var duration = 750,
+          delay = 25;
+
+      var partition = d3.partition()
+        .size([width/3, height/1.4])
+        .padding(0)
+        .round(true);
+          // .value(function(d) { return d.size; });
+
+      var xAxis = d3.axisBottom(x)
+                .scale(x);
+
+      var svg = d3.select("#hierarchy_barchart").append("svg")
+          .attr("width", width/3)
+          .attr("height", height/1.4)
+          .attr("id","barchart")
+        .append("g")
+          .attr("transform", "translate(" + "10,10)");
+
+      svg.append("rect")
+          .attr("class", "background")
+          .attr("width", width/3)
+          .attr("height", height/1.4)
+          .on("click", up);
+
+      svg.append("g")
+          .attr("class", "x axis");
+
+      svg.append("g")
+          .attr("class", "y axis")
+        .append("line")
+          .attr("y1", "100%");
+
+      d3.json(data_file_path, function(error, root) {
+        if (error) throw error;
+
+        root = d3.hierarchy(root)
+        .sum(function(d) { return d.size; })
+        .sort(function(a, b) { return b.value - a.value; });
+        partition(root);
+
+        x.domain([0, root.value]).nice();
+        down(root, 0);
+      });
+
+      function down(d, i) {
+        if (!d.children || this.__transition__) return;
+        var end = duration + d.children.length * delay;
+
+        // Mark any currently-displayed bars as exiting.
+        var exit = svg.selectAll(".enter")
+            .attr("class", "exit");
+
+        // Entering nodes immediately obscure the clicked-on bar, so hide it.
+        exit.selectAll("rect").filter(function(p) { return p === d; })
+            .style("fill-opacity", 1e-6);
+
+        // Enter the new bars for the clicked-on data.
+        // Per above, entering bars are immediately visible.
+        var enter = bar(d)
+            .attr("transform", stack(i))
+            .style("opacity", 1);
+
+        // Have the text fade-in, even though the bars are visible.
+        // Color the bars as parents; they will fade to children if appropriate.
+        enter.select("text").style("fill-opacity", 1e-6);
+        enter.select("rect").style("fill", color(true));
+
+        // Update the x-scale domain.
+        x.domain([0, d3.max(d.children, function(d) { return d.value; })]).nice();
+
+        // Update the x-axis.
+        svg.selectAll(".x.axis").transition()
+            .duration(duration)
+            .call(xAxis);
+
+        // Transition entering bars to their new position.
+        var enterTransition = enter.transition()
+            .duration(duration)
+            .delay(function(d, i) { return i * delay; })
+            .attr("transform", function(d, i) { return "translate(0," + barHeight * i * 1.2 + ")"; });
+
+        // Transition entering text.
+        enterTransition.select("text")
+            .style("fill-opacity", 1);
+
+        // Transition entering rects to the new x-scale.
+        enterTransition.select("rect")
+            .attr("width", function(d) { return x(d.value); })
+            .style("fill", function(d) { return color(!!d.children); });
+
+        // Transition exiting bars to fade out.
+        var exitTransition = exit.transition()
+            .duration(duration)
+            .style("opacity", 1e-6)
+            .remove();
+
+        // Transition exiting bars to the new x-scale.
+        exitTransition.selectAll("rect")
+            .attr("width", function(d) { return x(d.value); });
+
+        // Rebind the current node to the background.
+        svg.select(".background")
+            .datum(d)
+          .transition()
+            .duration(end);
+
+        d.index = i;
+      }
+
+      function up(d) {
+        if (!d.parent || this.__transition__) return;
+        var end = duration + d.children.length * delay;
+
+        // Mark any currently-displayed bars as exiting.
+        var exit = svg.selectAll(".enter")
+            .attr("class", "exit");
+
+        // Enter the new bars for the clicked-on data's parent.
+        var enter = bar(d.parent)
+            .attr("transform", function(d, i) { return "translate(0," + barHeight * i * 1.2 + ")"; })
+            .style("opacity", 1e-6);
+
+        // Color the bars as appropriate.
+        // Exiting nodes will obscure the parent bar, so hide it.
+        enter.select("rect")
+            .style("fill", function(d) { return color(!!d.children); })
+          .filter(function(p) { return p === d; })
+            .style("fill-opacity", 1e-6);
+
+        // Update the x-scale domain.
+        x.domain([0, d3.max(d.parent.children, function(d) { return d.value; })]).nice();
+
+        // Update the x-axis.
+        svg.selectAll(".x.axis").transition()
+            .duration(duration)
+            .call(xAxis);
+
+        // Transition entering bars to fade in over the full duration.
+        var enterTransition = enter.transition()
+            .duration(end)
+            .style("opacity", 1);
+
+        // Transition entering rects to the new x-scale.
+        // When the entering parent rect is done, make it visible!
+        enterTransition.select("rect")
+            .attr("width", function(d) { return x(d.value); })
+            .each("end", function(p) { if (p === d) d3.select(this).style("fill-opacity", null); });
+
+        // Transition exiting bars to the parent's position.
+        var exitTransition = exit.selectAll("g").transition()
+            .duration(duration)
+            .delay(function(d, i) { return i * delay; })
+            .attr("transform", stack(d.index));
+
+        // Transition exiting text to fade out.
+        exitTransition.select("text")
+            .style("fill-opacity", 1e-6);
+
+        // Transition exiting rects to the new scale and fade to parent color.
+        exitTransition.select("rect")
+            .attr("width", function(d) { return x(d.value); })
+            .style("fill", color(true));
+
+        // Remove exiting nodes when the last child has finished transitioning.
+        exit.transition()
+            .duration(end)
+            .remove();
+
+        // Rebind the current parent to the background.
+        svg.select(".background")
+            .datum(d.parent)
+          .transition()
+            .duration(end);
+      }
+
+      // Creates a set of bars for the given data node, at the specified index.
+      function bar(d) {
+        var bar = svg.insert("g", ".y.axis")
+            .attr("class", "enter")
+            .attr("transform", "translate(0,5)")
+          .selectAll("g")
+            .data(d.children)
+          .enter().append("g")
+            .style("cursor", function(d) { return !d.children ? null : "pointer"; })
+            .on("click", down);
+
+        bar.append("text")
+            .attr("x", -6)
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d.name; });
+
+        bar.append("rect")
+            .attr("width", function(d) { return x(d.value); })
+            .attr("height", barHeight);
+
+        return bar;
+      }
+
+      // A stateful closure for stacking bars horizontally.
+      function stack(i) {
+        var x0 = 0;
+        return function(d) {
+          var tx = "translate(" + x0 + "," + barHeight * i * 1.2 + ")";
+          x0 += x(d.value);
+          return tx;
+        };
+      }
 }
